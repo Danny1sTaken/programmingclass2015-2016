2621dc3362dce18df82fa97bc17625cef6982c4a
Danny1sTaken <dh.hernandez433@gmail.com>
2015-09-18 10:17:40 -0500

    Create assighnment4.rb

---
+++ assighnment4.rb
@@ -0,0 +1,7 @@
+puts "how many hours do i work?"
+hours = gets.to_i
+puts "how much do you get per hour?"
+money = gets.to_i
+puts "how many weeks do i work?"
+weeks = gets.to_i
+salary = hours * money * weeks
